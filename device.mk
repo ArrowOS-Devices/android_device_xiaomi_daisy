e625279a32302723f06eff12eeedf35d10023122
acras01 <acras1@gmail.com>
2020-01-26 11:40:39 +0200

    daisy: Switch to basic usb hal

--- device.mk
+++ device.mk
@@ -421,7 +421,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.xiaomi_8996
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
