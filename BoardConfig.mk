4d4d934c7dbd48ed3da69f9d70b95e504cbc4d51
acras01 <acras1@gmail.com>
2020-01-26 17:04:28 +0200

    daisy: use aosp surfaceflinger

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,6 +186,9 @@ TARGET_PER_MGR_ENABLED := true
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/wakeup_gesture"
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
 
