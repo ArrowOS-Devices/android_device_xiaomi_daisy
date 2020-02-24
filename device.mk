8c392d88d541a4be000d71ac6dd434dee47fa5b6
acras01 <acras1@gmail.com>
2020-02-24 09:01:27 +0200

    daisy: Remove obsolete packages

--- device.mk
+++ device.mk
@@ -472,13 +472,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# Wallpapers
-PRODUCT_PACKAGES += \
-    PixelLiveWallpaperPrebuilt
-
 # Wi-Fi Display
 PRODUCT_BOOT_JARS += \
     WfdCommon
-
-# Include AmbientSense if it's available
--include vendor/ambientmusic/AmbientMusic.mk
