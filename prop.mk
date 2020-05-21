defb0e02bd7a121655ed5cb5603aefcc86c7b327
vitor00almei <pondebatata@outlook.com>
2020-05-21 15:54:20 -0300

    daisy: Improve HWUI
    Edit some hw and surfaceflinger props to improve HWUI

--- prop.mk
+++ prop.mk
@@ -128,6 +128,7 @@ persist.hwc.enable_vds=1 \
 persist.hwc.mdpcomp.enable=true \
 ro.opengles.version=196610 \
 ro.qualcomm.cabl=0 \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
 ro.sf.lcd_density=420 \
 debug.sdm.support_writeback=0 \
 ro.vendor.display.cabl=2 \
@@ -287,13 +288,14 @@ persist.vendor.radio.data_con_rprt=1 \
 persist.sys.fflag.override.settings_network_and_internet_v2=true
 
 # SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-ro.surface_flinger.max_virtual_display_dimension=4096 \
+PRODUCT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.protected_contents=true \
-ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
-ro.surface_flinger.use_color_management=true
+debug.sf.early_phase_offset_ns=1500000 \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_virtual_display_dimension=4096
 
 # Thermal configs path
 PRODUCT_PROPERTY_OVERRIDES += \
