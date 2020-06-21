385f2cdfe23eda84814e02980d7236c735840902
acras01 <acras1@gmail.com>
2020-06-21 13:31:52 +0300

    daisy: Fix vanilla builds bootloop

--- device.mk
+++ device.mk
@@ -19,9 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-ifeq ($(CURRENT_BUILD_TYPE),gapps)
+#ifeq ($(CURRENT_BUILD_TYPE),gapps)
     $(call inherit-product, vendor/google-customization/config.mk)
-endif
+#endif
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
