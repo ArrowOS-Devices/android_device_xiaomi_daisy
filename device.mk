84bf088bb12b53fb779d82ec607ce852038c97b3
acras01 <acras1@gmail.com>
2020-07-09 11:06:20 +0300

    daisy: Remove unused audio packages, switch to soundtrigger 2.2

--- device.mk
+++ device.mk
@@ -134,16 +134,11 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
     audio.r_submix.default \
     audio.usb.default \
-    libaudiohal \
     libaudioroute \
     libaacwrapper \
     libaudio-resampler \
