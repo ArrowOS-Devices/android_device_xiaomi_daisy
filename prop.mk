1a258d7acc610ff56b9b9bf57a6679c75420251c
acras01 <acras1@gmail.com>
2020-07-20 08:55:11 +0300

    daisy: prop: Modify HAL1 package list

--- prop.mk
+++ prop.mk
@@ -173,7 +173,7 @@ vidc.enc.dcvs.extra-buff-count=2 \
 vendor.camera.lowpower.record.enable=1 \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.huaqin.factory,org.lineageos.snap \
 vendor.camera.aux.packagelist2=com.android.systemui,com.huaqin.cameraautotest,com.huaqin.runtime \
-vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk,com.whatsapp,com.android.camera2
+vendor.camera.hal1.packagelist=com.skype.raider,com.viber.voip,com.whatsapp,com.instagram.android,org.telegram.messenger
 
 # Cne
 PRODUCT_PROPERTY_OVERRIDES += \
