098ebf6417ce2c681c3b204c197b05a87a244bc1
acras01 <acras1@gmail.com>
2020-09-22 16:05:29 +0300

    daisy: Update VNDK

--- device.mk
+++ device.mk
@@ -487,9 +487,11 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# VNDK-SP
+# VNDK
 PRODUCT_PACKAGES += \
-    vndk-sp
+    libicuuc.vendor \
+    libstdc++.vendor \
+    vndk_package
 
 # Whitelisted apps
 PRODUCT_COPY_FILES += \
