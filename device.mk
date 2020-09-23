fcd96a0d10fd9edecfcf873cf38b4314444dca49
acras01 <acras1@gmail.com>
2020-09-23 11:37:56 +0300

    daisy: Build vendor variant of libdng_sdk

--- device.mk
+++ device.mk
@@ -195,7 +195,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.postproc@1.0
+    vendor.qti.hardware.camera.postproc@1.0 \
+    libdng_sdk.vendor
 
 # Consumerir
 PRODUCT_PACKAGES += \
