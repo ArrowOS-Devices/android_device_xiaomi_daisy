b650bebbb12250db2c99c79aac9985de685da9f1
acras01 <acras1@gmail.com>
2020-10-13 19:38:29 +0300

    daisy: libhidl: libhidltransport -> libhidlbase

--- device.mk
+++ device.mk
@@ -286,10 +286,10 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0.vendor
-
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # HW crypto
 PRODUCT_PACKAGES += \
