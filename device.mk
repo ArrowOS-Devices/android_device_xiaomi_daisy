8205fde26756dfce01a469f0bdfd8811a0814f37
acras01 <acras1@gmail.com>
2020-10-13 19:17:39 +0300

    daisy: Remove xiaomi offline charging daemon

--- device.mk
+++ device.mk
@@ -277,10 +277,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
-# Healthd
-PRODUCT_PACKAGES += \
-    chargeonlymode
-
 # Health HAL
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
