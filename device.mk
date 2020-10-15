bf65d199e9a6131bb36e833111246fe968f02609
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-15 14:56:51 +0300

    daisy: add public.libraries.txt to device tree

--- device.mk
+++ device.mk
@@ -395,6 +395,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+	
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
