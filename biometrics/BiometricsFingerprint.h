0d131ac2896f63295a6f63b55ee450b11a469cff
acras01 <acras1@gmail.com>
2020-10-26 08:44:26 +0200

    daisy: Update biometrics from https://github.com/SakilMondal/android_device_xiaomi_sdm660-common/tree/lineage-18.0/biometrics

--- biometrics/BiometricsFingerprint.h
+++ biometrics/BiometricsFingerprint.h
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
- * Copyright (C) 2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,24 +17,13 @@
 #ifndef ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_1_BIOMETRICSFINGERPRINT_H
 #define ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_1_BIOMETRICSFINGERPRINT_H
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.xiaomi"
-
-#include "fingerprint.h"
-
+#include <log/log.h>
 #include <android/log.h>
-#include <cutils/properties.h>
 #include <hardware/hardware.h>
-#include <hardware/hw_auth_token.h>
-#include <hidl/HidlTransportSupport.h>
+#include <hardware/fingerprint.h>
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
-#include <inttypes.h>
-#include <log/log.h>
-#include <unistd.h>
-
 #include <android/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
-
-#include <vendor/xiaomi/hardware/fingerprintextension/1.0/IXiaomiFingerprint.h>
 
 namespace android {
 namespace hardware {
@@ -52,15 +40,11 @@ using ::android::hardware::Void;
 using ::android::hardware::hidl_vec;
 using ::android::hardware::hidl_string;
 using ::android::sp;
-using ::android::status_t;
 
-using ::vendor::xiaomi::hardware::fingerprintextension::V1_0::IXiaomiFingerprint;
-
-struct BiometricsFingerprint : public IBiometricsFingerprint, public IXiaomiFingerprint {
+struct BiometricsFingerprint : public IBiometricsFingerprint {
+public:
     BiometricsFingerprint();
     ~BiometricsFingerprint();
-
-    status_t registerAsSystemService();
 
     // Method to wrap legacy HAL with BiometricsFingerprint class
     static IBiometricsFingerprint* getInstance();
@@ -77,8 +61,7 @@ struct BiometricsFingerprint : public IBiometricsFingerprint, public IXiaomiFing
     Return<RequestStatus> setActiveGroup(uint32_t gid, const hidl_string& storePath) override;
     Return<RequestStatus> authenticate(uint64_t operationId, uint32_t gid) override;
 
-    Return<int32_t> extCmd(int32_t cmd, int32_t param) override;
-
+private:
     static fingerprint_device_t* openHal();
     static void notify(const fingerprint_msg_t *msg); /* Static callback for legacy HAL implementation */
     static Return<RequestStatus> ErrorFilter(int32_t error);
