c60eea9b3e81913c85102a27c6af98f86dc69047
acras01 <acras1@gmail.com>
2020-10-28 07:55:24 +0200

    daisy: Initial R sepolicy

--- device.mk
+++ device.mk
@@ -333,6 +333,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.daisy
 
+# MatLog app
+PRODUCT_PACKAGES += \
+    MatLog
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail_vendor
