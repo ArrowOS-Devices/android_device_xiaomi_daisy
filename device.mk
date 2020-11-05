410319f450805e1efb54447997726bb1a9a6ab58
acras01 <acras1@gmail.com>
2020-11-05 09:32:41 +0200

    daisy: Update audio to 6.0 (11)

--- device.mk
+++ device.mk
@@ -133,9 +133,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl \
     audio.r_submix.default \
