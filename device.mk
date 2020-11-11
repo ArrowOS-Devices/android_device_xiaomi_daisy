a2dd8620a810fa98bb6c27fa2191e2157b96eff7
acras01 <acras1@gmail.com>
2020-11-11 17:05:55 +0200

    daisy: Use QTI thermal HAL

--- device.mk
+++ device.mk
@@ -521,11 +521,10 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.xiaomi
+    android.hardware.thermal@2.0-service.qti
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
-    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # ThermalController app
 PRODUCT_PACKAGES += \
