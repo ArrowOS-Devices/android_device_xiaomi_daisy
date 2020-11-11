1fe357fab3dbc2f8b6315a66ca6f4b5cd11d9c23
acras01 <acras1@gmail.com>
2020-11-11 16:32:43 +0200

    daisy: Add back thermal-engine.conf

--- device.mk
+++ device.mk
@@ -524,6 +524,7 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.xiaomi
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # ThermalController app
