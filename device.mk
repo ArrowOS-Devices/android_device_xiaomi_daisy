a972f306069057fba6869a43a12734b9e6c9fa02
acras01 <acras1@gmail.com>
2020-12-09 08:33:44 +0200

    daisy: Add vendor.qti.hardware.display hals

--- device.mk
+++ device.mk
@@ -233,6 +233,17 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     vendor.display.config@1.12 \
     vendor.display.config@2.0 \
+    vendor.qti.hardware.display.allocator@1.0 \
+    vendor.qti.hardware.display.allocator@3.0 \
+    vendor.qti.hardware.display.composer@1.0 \
+    vendor.qti.hardware.display.composer@2.0 \
+    vendor.qti.hardware.display.mapper@1.0 \
+    vendor.qti.hardware.display.mapper@1.1 \
+    vendor.qti.hardware.display.mapper@2.0 \
+    vendor.qti.hardware.display.mapper@3.0 \
+    vendor.qti.hardware.display.mapper@4.0 \
+    vendor.qti.hardware.display.mapperextensions@1.0 \
+    vendor.qti.hardware.display.mapperextensions@1.1 \
     copybit.msm8953 \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
