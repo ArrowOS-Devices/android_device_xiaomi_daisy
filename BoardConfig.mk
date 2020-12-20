874d38e2760ecfed5c812b59e84ec59e12e86b49
acras01 <acras1@gmail.com>
2020-12-20 07:45:42 +0200

    daisy: Fix neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -250,7 +250,7 @@ BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_NO_RECOVERY := true
 
 # We modify several neverallows, so let the build proceed
-SELINUX_IGNORE_NEVERALLOWS := true
+#SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_VNDK_VERSION := current
