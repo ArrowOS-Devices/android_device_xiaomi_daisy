e89c104c2b7ce4943a7f251c82621a963148f113
acras01 <acras1@gmail.com>
2020-12-23 08:31:03 +0200

    daisy: Rework camera packages

--- device.mk
+++ device.mk
@@ -196,18 +196,12 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.cameraservice.common@2.0 \
-    android.frameworks.cameraservice.device@2.0 \
-    android.frameworks.cameraservice.service@2.0 \
-    android.frameworks.displayservice@1.0_32 \
+    android.hardware.camera.device@3.6 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
-    android.hardware.camera.provider@2.6 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.postproc@1.0 \
-    libdng_sdk.vendor \
-    android.hardware.camera.device@3.4
+    libdng_sdk.vendor
 
 # Component overrides
 PRODUCT_COPY_FILES += \
