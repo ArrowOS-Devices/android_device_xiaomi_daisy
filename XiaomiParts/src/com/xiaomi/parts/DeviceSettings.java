fca84289eaca90d3b1254eeeff09948bc2661151
acras01 <acras1@gmail.com>
2021-01-18 14:04:11 +0200

    daisy: XiaomiParts: remove Selinux switch

--- XiaomiParts/src/com/xiaomi/parts/DeviceSettings.java
+++ XiaomiParts/src/com/xiaomi/parts/DeviceSettings.java
@@ -21,7 +21,6 @@ import android.content.pm.PackageManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.SELinux;
 import android.os.Handler;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceManager;
@@ -35,9 +34,6 @@ import android.util.Log;
 
 import com.xiaomi.parts.preferences.SecureSettingListPreference;
 
-import com.xiaomi.parts.SuShell;
-import com.xiaomi.parts.SuTask;
-
 public class DeviceSettings extends PreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
@@ -46,15 +42,9 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String PREF_CAMERA = "camera";
     public static final String CAMERA_SYSTEM_PROPERTY = "persist.camera.profile";
 
-    private static final String SELINUX_CATEGORY = "selinux";
-    private static final String PREF_SELINUX_MODE = "selinux_mode";
-    private static final String PREF_SELINUX_PERSISTENCE = "selinux_persistence";
-
     private SecureSettingListPreference mCamera;
 
     private static Context mContext;
-    private SwitchPreference mSelinuxMode;
-    private SwitchPreference mSelinuxPersistence;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -63,16 +53,6 @@ public class DeviceSettings extends PreferenceFragment implements
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
 
         String device = FileUtils.getStringProp("ro.build.product", "unknown");
-
-        // SELinux
-        Preference selinuxCategory = findPreference(SELINUX_CATEGORY);
-        mSelinuxMode = (SwitchPreference) findPreference(PREF_SELINUX_MODE);
-        mSelinuxMode.setChecked(SELinux.isSELinuxEnforced());
-        mSelinuxMode.setOnPreferenceChangeListener(this);
-
-        mSelinuxPersistence =(SwitchPreference) findPreference(PREF_SELINUX_PERSISTENCE);
-        mSelinuxPersistence.setOnPreferenceChangeListener(this);
-        mSelinuxPersistence.setChecked(prefs.getBoolean(PREF_SELINUX_PERSISTENCE, false));
 
         // HAL3|HAL1 Switch button profiles
         mCamera = (SecureSettingListPreference) findPreference(PREF_CAMERA);
@@ -90,41 +70,11 @@ public class DeviceSettings extends PreferenceFragment implements
                	mCamera.setSummary(mCamera.getEntry());
                 FileUtils.setStringProp(CAMERA_SYSTEM_PROPERTY, (String) value);
                 break;
-            case PREF_SELINUX_MODE:
-                new SwitchSelinuxTask(getActivity()).execute((Boolean) value);
-                break;
             default:
                 break;
         }
         return true;
     }
-
-        private class SwitchSelinuxTask extends SuTask<Boolean> {
-          public SwitchSelinuxTask(Context context) {
-            super(context);
-          }
-          @Override
-          protected void sudoInBackground(Boolean... params) throws SuShell.SuDeniedException {
-            if (params.length != 1) {
-              Log.e(TAG, "SwitchSelinuxTask: invalid params count");
-              return;
-            }
-            if (params[0]) {
-              SuShell.runWithSuCheck("setenforce 1");
-            } else {
-              SuShell.runWithSuCheck("setenforce 0");
-            }
-          }
-
-          @Override
-          protected void onPostExecute(Boolean result) {
-            super.onPostExecute(result);
-            if (!result) {
-              // Did not work, so restore actual value
-              mSelinuxMode.setChecked(!mSelinuxMode.isChecked());
-            }
-          }
-        }
 
     private boolean isAppNotInstalled(String uri) {
         PackageManager packageManager = getContext().getPackageManager();
