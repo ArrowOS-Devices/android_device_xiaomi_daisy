8ad54b722db7600e2dab5c4a5db3df8c9e1bf019
acras01 <acras1@gmail.com>
2021-01-19 14:10:59 +0200

    daisy: Build android.hardware.camera.provider@2.4 package

--- device.mk
+++ device.mk
@@ -201,6 +201,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6 \
+    android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
