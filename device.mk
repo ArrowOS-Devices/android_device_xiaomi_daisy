bce36936183580083825c16493342c284ca8b71d
acras01 <acras1@gmail.com>
2021-01-20 14:39:16 +0200

    daisy: Remove system version of bt interfaces

--- device.mk
+++ device.mk
@@ -158,11 +158,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0-service\
     android.hardware.bluetooth.audio@2.0 \
     android.hardware.bluetooth.audio@2.0-impl:32 \
-    com.qualcomm.qti.bluetooth_audio@1.0 \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0 \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.bluetooth_dun@1.0 \
     vendor.qti.hardware.bluetooth_dun@1.0.vendor \
     libbluetooth_audio_session
 
@@ -193,9 +190,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     bt_did.conf \
     bt_stack.conf \
-    vendor.qti.hardware.btconfigstore@1.0 \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0 \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
 
 # Camera
