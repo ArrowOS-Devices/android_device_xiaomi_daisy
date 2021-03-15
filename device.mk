55b72db3344a3ecfc3ff965f7e14a05ca570deaa
acras01 <acras1@gmail.com>
2021-03-15 15:24:54 +0200

    daisy: Add android.hardware.audio.effect@2.0-service to device.mk

--- device.mk
+++ device.mk
@@ -136,6 +136,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl \
     audio.r_submix.default \
     audio.usb.default \
