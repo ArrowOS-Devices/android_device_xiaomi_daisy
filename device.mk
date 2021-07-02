2171fea77bf0478493c1d04e836618d1ffbed97e
acras01 <acras1@gmail.com>
2021-07-02 09:31:23 +0300

    daisy: Add copying of excluded input devices list for vibrator hal

--- device.mk
+++ device.mk
@@ -559,6 +559,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 29
 
