f5ba07abc4b6c0a3602a2d9c7e97529a5f819c7f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-09 12:40:03 +0200

    daisy: Get rid of legacy audio policy conf
    Change-Id: Iafefa425bd480e0fcfe4f9614f86e91676cd1399

--- device.mk
+++ device.mk
@@ -175,7 +175,6 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
