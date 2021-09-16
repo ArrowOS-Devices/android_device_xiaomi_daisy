fe046372750e0e6364531b2dc8a157940659bf36
acras01 <acras1@gmail.com>
2021-09-16 08:36:20 +0300

    daisy: ir: Rebrand to daisy

--- device.mk
+++ device.mk
@@ -212,7 +212,7 @@ PRODUCT_PACKAGES += \
 
 # ConsumerIR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi_msm8953
+    android.hardware.ir@1.0-service.xiaomi_daisy
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
