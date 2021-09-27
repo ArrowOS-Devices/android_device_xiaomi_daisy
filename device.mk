b99b4ff16da47f0467050413647fea0e358a20bc
acras01 <acras1@gmail.com>
2021-09-27 11:59:53 +0300

    daisy: Build vendor variant of vendor.display.config@1.0

--- device.mk
+++ device.mk
@@ -214,6 +214,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    vendor.display.config@1.0.vendor \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
