0bd5716743a1dd8be040639df77be518ef6383f3
acras01 <acras1@gmail.com>
2021-10-15 08:55:10 +0300

    daisy: Delete dup

--- device.mk
+++ device.mk
@@ -127,7 +127,6 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
