f0a7846104556db1bac2e1b1b600632976a429b9
acras01 <acras1@gmail.com>
2021-11-08 08:27:07 +0200

    Revert "daisy: ir: Binderize"
    This reverts commit a9f6ff207501d042a7b17f286c779e9c30b5d24a.

--- ir/service.cpp
+++ ir/service.cpp
@@ -8,30 +8,10 @@
 
 #include <android/hardware/ir/1.0/IConsumerIr.h>
 #include <hidl/LegacySupport.h>
-#include "ConsumerIr.h"
 
 using android::hardware::ir::V1_0::IConsumerIr;
-using android::hardware::ir::V1_0::implementation::ConsumerIr;
-using android::hardware::configureRpcThreadpool;
-using android::hardware::joinRpcThreadpool;
+using android::hardware::defaultPassthroughServiceImplementation;
 
 int main() {
-    android::sp<ConsumerIr> service = new ConsumerIr();
-
-    if (!service->isOk()) {
-        ALOGE("Instance of ConsumerIr failed to init");
-        return 1;
-    }
-
-    configureRpcThreadpool(1, true /*callerWillJoin*/);
-
-    android::status_t status = service->registerAsService();
-    if (status != android::OK) {
-        ALOGE("Cannot register ConsumerIr service");
-        return 1;
-    }
-
-    joinRpcThreadpool();
-
-    return 0; // should never get here
+    return defaultPassthroughServiceImplementation<IConsumerIr>();
 }
