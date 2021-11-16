9eae9a936fc76a47a954989966c0a120358c87ac
acras01 <acras1@gmail.com>
2021-11-16 09:57:55 +0200

    Revert "daisy: QCamera2: HAL3: Fix missing pthread unlock"
    This reverts commit 08fc813e669d8719c0825e36e83f92753117a0f4.

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -4839,7 +4839,6 @@ no_error:
         if (channel == NULL) {
             LOGE("invalid channel pointer for stream");
             assert(0);
-            pthread_mutex_unlock(&mMutex);
             return BAD_VALUE;
         }
 
@@ -4898,7 +4897,6 @@ no_error:
         } else {
             LOGE("Internal requests not supported on this stream type");
             assert(0);
-            pthread_mutex_unlock(&mMutex);
             return INVALID_OPERATION;
         }
         latestRequest->internalRequestList.push_back(requestedStream);
